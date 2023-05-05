831ad8bd2cf11a2fdd0f730e736a2af0c9051098
tmannazy <e.anene@hotmail.com>
2023-05-05 17:49:39 +0100

    Initial commit

---
+++ src/main/java/africa/auth/ldap/HomeController.java
@@ -0,0 +1,12 @@
+package africa.auth.ldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to Home Page!";
+    }
+}
---
+++ src/test/java/africa/auth/ldap/LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.auth.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
